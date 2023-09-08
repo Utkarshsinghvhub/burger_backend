5175e485ab489ab5b7c7292cb54f45273401c918
utkarsh singh <utkarshsingh17082000@gmail.com>
2023-09-09 02:36:17 +0530

    final commit

---
+++ app.js
@@ -0,0 +1,64 @@
+import express,{urlencoded} from "express";
+import dotenv from "dotenv";
+import {connectPassport} from "./utils/Provider.js"
+import session from "express-session";
+import passport from "passport";
+import cookieParser from "cookie-parser";
+import { errorMiddleware } from "./middlewares/errorMiddleware.js";
+import cors from "cors";
+
+const app=express();
+export default app;
+
+//for connection
+dotenv.config({
+    path:"./config/config.env",
+})
+
+// Using Middlewares
+app.use(
+    session({
+      secret: process.env.SESSION_SECRET,
+      resave: false,
+      saveUninitialized: false,
+    })
+);
+
+
+
+app.use(cookieParser());
+app.use(express.json());
+app.use(
+  urlencoded({
+    extended: true,
+  })
+);
+
+
+app.use(
+  cors({
+    credentials: true,
+    origin: process.env.FRONTEND_URL,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+  })
+);
+
+app.use(passport.authenticate("session"));
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+connectPassport();
+
+//import routes
+import userRoute from "./routes/user.js";
+import orderRoute from "./routes/order.js";
+
+app.use("/api/v1",userRoute);
+app.use("/api/v1",orderRoute);
+
+
+
+
+// Using Error Middleware
+app.use(errorMiddleware);
